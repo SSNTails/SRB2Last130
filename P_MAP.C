c51788fee83acaca37d31e82d5ad1066ad9d6db9
HalfHaIf <69714108+HalfHaIf@users.noreply.github.com>
2025-06-22 11:59:47 +1000

    Fixed Skim

--- P_MAP.C
+++ P_MAP.C
@@ -783,9 +783,10 @@ if(tmthing->player) // Is the moving/interacting object the player?
 				case MT_MISC10: // 10-Ring box
 				case MT_MISC11: // 25-Ring box
 				case MT_INV: // Invincibility box
-				case MT_BLUECRAWLA:
+				case MT_BLUECRAWLA: 
 				case MT_REDCRAWLA:
 				case MT_GFZFISH:
+				case MT_SKIM:
 				case MT_JETTBOMBER:
 				case MT_JETTGUNNER:
 					if(((tmthing->player->mfjumped == 0) && (tmthing->player->mfspinning == 0)) && ((tmthing->player->powers[pw_super]) || (tmthing->player->powers[pw_invulnerability]))) // Don't bust boxes like Sonic Adventure Tails 11-02-2000
